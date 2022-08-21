b38aebdb7b5a04522db4b881b2fb52a11507810e
ashokamarthuluri <amarthaluriashok@gmail.com>
2022-08-22 01:11:53 +0530

    added region names to AWS_services

--- task1.py
+++ task1.py
@@ -1,6 +1,6 @@
 import boto3
 
-ec2 = boto3.resource('ec2')
+ec2 = boto3.resource('ec2', region_name='us-east-1')
 
 win_ec2_list = []
 
@@ -12,7 +12,7 @@ for i in ec2.instances.all():
 
 #Sending powershell command to get name and version of the software.
 #Using AWS SSM for connecting to the server for above. And Saving the output in S3 Bucket. 
-ssm_client = boto3.client('ssm')
+ssm_client = boto3.client('ssm',region_name='us-east-1')
 response = ssm_client.send_command(
     InstanceIds = win_ec2_list,
     DocumentName = "AWS-RunPowerShellScript",
@@ -32,7 +32,7 @@ output = ssm_client.get_command_invocation(
 # print(output)
 
 #Retreving the Output and printing the Output form S3 bucket.
-s3_res = boto3.resource('s3')
+s3_res = boto3.resource('s3',region_name='us-east-1')
 my_bucket = s3_res.Bucket('ssmbucketfortst')
 
 for i in my_bucket.objects.all():
